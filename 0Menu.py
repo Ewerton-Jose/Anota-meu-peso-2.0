8a02b847d58bc30e65e5263601381e6782fdd6d5
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2021-04-20 20:57:20 -0300

    junção
    * todos os códigos vão ficar em um só comando
    * IMC avançou

--- 0Menu.py
+++ 0Menu.py
@@ -1,8 +1,45 @@
-from aFunções import *
+# ------------- Funções ------------
+
+def lin(x = '-', y = 40):
+    print(x * y)
+
+def cabeçalho(x = "nome"):
+    lin()
+    print(f'{x:-^40}')
+    lin()
+
+def menu(x = []):
+    s = 1
+    for e in x:
+        print(f'Opção {s} = {e}')
+        s += 1
+    lin()
+
+def analiseInteiro(nome = 'str'):
+    while True:
+        try:
+            n = int(input(f'{nome}'))
+        except:
+            print('\033[31mErro, numero inválido\033[m')
+        else:
+            return n
+            break
+
+def analiseFloat(nome = 'str'):
+    while True:
+        try:
+            n = float(input(f"{nome}"))
+        except:
+            print('\033[31mErro, Numero inválido\033[m')
+        else:
+            return n
+            break
+    
+# ------------------ Memu --------------------
 
 cabeçalho("Menu")
 op = ['Exercícios Diarios', 'Cronograma da alimentação', 'IMC',"Registra Meu Peso",'litros diários']
-menuo(op)
+menu(op)
 escolha = analiseInteiro('Sua escolha: ')
 while escolha < 1 or escolha > 5:
     print('\033[31mopção inválida\033[m')
@@ -12,7 +49,29 @@ if escolha == 1:
     print('x1')
 elif escolha == 2:
     print('x2')
+
+# ------------------ IMC ---------------------- 
 elif escolha == 3:
+    cabeçalho("Veja com está o seu IMC")
+    Altura = analiseFloat('Digite sua altura: ')
+    Peso = analiseFloat('Digite seu peso: ')
+    IMC = Peso / (Altura * Altura)
+    Classificação = ""
+    if IMC < 18.5:
+        Classificação = "Magreza"
+    elif IMC > 18.5 and IMC < 24.9:
+        Classificação = "Normal"
+    elif IMC > 24.9 and IMC < 29.9:
+        Classificação = "acima do peso"
+    elif IMC > 30 and IMC < 39.9:
+        Classificação = "Obsidade"
+    elif IMC > 39.9:
+        Classificação = "Obsidade morbida"
+
+    print(f'Seu IMC é de {IMC} e sua classificação é {Classificação}')
+
+    print(f'{IMC}')
+
     print('x3')
 elif escolha == 4:
     print('x4')
